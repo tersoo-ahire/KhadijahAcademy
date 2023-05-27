f4681decf7e2da08ac3ec7d4d43e709e28086b10
Tersoo <tersooahire21@gmail.com>
2023-05-27 14:36:06 +0100

    added hero section

--- project/src/components/Navbar.jsx
+++ project/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import Cart from "../assets/cart.svg";
 
 export default function Navbar(){
     return (
         <>
-        
+            <nav>
+                <div className="nav-left">
+                    <img src="./graduation-hat.png" alt="Graduation Hat" />
+                    <p>KhadijahAcademy</p>
+                </div>
+                <div className="nav-right">
+                    <Link to="/">Home</Link>
+                    <Link to="#">About Us</Link>
+                    <Link to="#">Courses</Link>
+                    <Link to="#" style={{display:"flex",gap:"0.5em"}}>
+                        <img src={Cart} alt="Cart Icon" style={{width:"20px"}}/>
+                        Cart
+                    </Link>
+                    <Link to="#">
+                        <button className="button-1">Sign In</button>
+                    </Link>
+                    <Link to="#">
+                        <button className="button-2">Join Academy</button>
+                    </Link>
+
+                </div>
+            </nav>
         </>
     )
 }
--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import '../styles_desktop/App.css';
-
+import Search from "../assets/search.svg";
+import Schoolgirl from "../assets/schoolgirl.png";
 
 export default function Home(){
     return (
         <>
-            {/* <h1>Home page</h1> */}
+            <section className="hero">
+                <div className="hero-left">
+                    <h1 className="hero-h1">Learn and practice at your own pace</h1>
+                    <p className="hero-p1">We are here to help you achieve that journey of a seamless online learning.</p>
+                    <div className="search-container">
+                        <img src={Search} alt="Search Icon" />
+                        <input type="search" placeholder="Search for courses" />
+                        <input id="search-button"  type="submit" value="Search" />
+                    </div>
+                </div>
+                <div className="hero-right">
+                    <img src={Schoolgirl} alt="Image of School girl"/>
+                </div>
+            </section>
         </>
     )
 }
