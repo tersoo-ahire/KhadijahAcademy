a48813754c8d1780e4f1e832709c037a201feca4
Tersoo <tersooahire21@gmail.com>
2023-05-27 10:54:49 +0100

    added routes from navigation

---
+++ project/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles_desktop/App.css';
+
+
+export default function Home(){
+    return (
+        <>
+            {/* <h1>Home page</h1> */}
+        </>
+    )
+}
