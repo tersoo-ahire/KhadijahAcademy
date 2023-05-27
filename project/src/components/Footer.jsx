5ee8bb7313764305ca6cf0e1382af264c6f11853
Tersoo <tersooahire21@gmail.com>
2023-05-27 18:35:17 +0100

    added footer

--- project/src/components/Footer.jsx
+++ project/src/components/Footer.jsx
@@ -2,8 +2,23 @@ import React from "react";
 
 export default function Footer(){
     return (
-        <>
-            {/* <h5>Footer</h5> */}
-        </>
+        <footer>
+            <div className="logo-container">
+                <img src="./graduation-hat.png" alt="Graduation Hat" />
+                <p>KhadijahAcademy</p>
+                <div className="description">Online course learning platform</div>
+            </div>
+            <h3>Subscribe to get our Newsletter</h3>
+            <div className="email-container">
+                <input className="email" type="email" placeholder="Your Email" />
+                <input className="subscribe" type="button" value="Subscribe" />
+            </div>
+            <div className="links">
+                <a href="#" style={{borderRight:"1px solid grey"}}>Careers</a>
+                <a href="#" style={{borderRight:"1px solid grey"}}>Privacy Policy</a>
+                <a href="#">Terms and Conditions</a>
+            </div>
+            <p className="copywrite">© 2023 KhadijatAcademy</p>
+        </footer>
     )
 }
